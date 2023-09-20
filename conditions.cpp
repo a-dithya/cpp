df946412f034424da45534893e2758aef83da92b
a-dithya <79012044+a-dithya@users.noreply.github.com>
2023-09-20 13:13:38 +0530

    Add files via upload

---
+++ conditions.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  /*  int age;
+    cin>>age;
+    if(age>=18){
+        cout<<"adult";
+    } else{
+        cout<<"not an adult";
+    }*/
+    int marks;
+    cin>>marks;
+     //if you write multiple if, even though condition checks 
+     //in first if then also it execute everything whereas else if won't check everything
+    /*if(marks<25){   
+        cout<<"F";
+    }else if(marks<=44){
+        cout<<"E";
+    }else if(marks>=45 and marks<=49){
+        cout<<"D";
+    }else if(marks>=50 and marks<=59){
+        cout<<"C";
+    }*/
+
+//~~~~~~Switch Statement~~~~~~~~~~~~~~
+
+    /*int day;
+    cin>>day;
+    switch(day){
+        case 1:cout<<"Monday";
+        break; //if no break stmnt it execute every case from matching conditions
+        default:cout<<"Wrong input";
+    }*/
+    return 0;
+}
